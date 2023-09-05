11e6b61d9a0872a9eb0eb89bc22be962dab473e6
YeLuo <19807116985@163.com>
2023-09-05 11:58:19 +0800

    correct InsertionSort

--- InsertionSort.c
+++ InsertionSort.c
@@ -4,7 +4,7 @@ void InsertionSort(int* arr,int length){
     if(arr==NULL||length<2)
         return;
     for(int i=1;i<length;i++){
-        for(int j=i-1;j>=0;j--){
+        for(int j=i-1;j>=0 && arr[j]>arr[j+1];j--){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j+1];
                 arr[j+1]=arr[j];
