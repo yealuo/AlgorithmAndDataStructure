4aac3a8b3d92986358466be3fd8eefbae5c7b7b3
YeLuo <19807116985@163.com>
2023-09-10 19:15:23 +0800

    优化二分查找

--- 1/BisectionMethed1.c
+++ 1/BisectionMethed1.c
@@ -7,16 +7,18 @@ void bisection(int* arr, size_t length, int target) {
         if (arr[mid] == target) {
             printf("已找到，该数的索引值为：%d", mid);
             break;
-        } else if (arr[mid] > target)
+        }
+        else if (arr[mid] > target)
             right = mid - 1;
         else if (arr[mid] < target)
             left = mid + 1;
     }
+    printf("未找到，该书不在此数组。");
 }
 
 int main() {
     int array[] = {1, 3, 7, 9, 10, 16, 23, 33, 36, 38, 40, 42, 49, 56, 57, 59};
     int length = sizeof(array) / sizeof(array[0]);
-    bisection(array, length, 57);
+    bisection(array, length, 58);
     return 0;
 }
