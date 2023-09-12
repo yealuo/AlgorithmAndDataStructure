21eab2f2479adb1414d3355d4cbf0a433a8cdabf
YeLuo <19807116985@163.com>
2023-09-12 11:49:22 +0800

    局部最小值问题

--- 1/BisectionMethed1.c
+++ 1/BisectionMethed1.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
+// 在一个有序数组中，找某个数是否存在
 void bisection1(int* arr, size_t length, int target) {
     int left = 0, right = length - 1;
     while (left <= right) {
         int mid = (left + right) >> 1;
         if (arr[mid] == target) {
             printf("已找到，该数的索引值为：%d", mid);
-            break;
-        }
-        else if (arr[mid] > target)
+            return;
+        } else if (arr[mid] > target) {
             right = mid - 1;
-        else if (arr[mid] < target)
+        } else if (arr[mid] < target) {
             left = mid + 1;
+        }
     }
     printf("未找到，该数不在此数组。");
+    return;
 }
 
 int main() {
     int array[] = {1, 3, 7, 9, 10, 16, 23, 33, 36, 38, 40, 42, 49, 56, 57, 59};
-    int length = sizeof(array) / sizeof(array[0]);
-    bisection1(array, length, 58);
+    size_t length = sizeof(array) / sizeof(array[0]);
+    bisection1(array, length, 57);
     return 0;
 }
--- 1/BisectionMethod2.c
+++ 1/BisectionMethod2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// 在一个有序数组中，找>=某个数最左侧的位置
 void bisection2(int* arr, size_t length, int target) {
     int left = 0, right = length - 1, t = -1;
     while (left <= right) {
@@ -7,18 +8,22 @@ void bisection2(int* arr, size_t length, int target) {
         if (arr[mid] >= target) {
             t = mid;
             right = mid - 1;
-        } else
+        } else {
             left = mid + 1;
+        }
     }
-    if (t < 0)
+    if (t < 0) {
         printf("未找到，该数不在此数组。");
-    else
+        return;
+    } else {
         printf("已找到，≥%d最左侧位置索引值为：%d", target, t);
+        return;
+    }
 }
 
 int main() {
     int array[] = {1, 3, 7, 9, 10, 16, 23, 33, 36, 38, 40, 42, 49, 56, 57, 59};
-    int length = sizeof(array) / sizeof(array[0]);
+    size_t length = sizeof(array) / sizeof(array[0]);
     bisection2(array, length, 16);
     return 0;
 }
