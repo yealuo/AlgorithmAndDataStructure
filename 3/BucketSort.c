3451cbbce8e3d91a3363280b4823c7304bc6dedb
YeLuo <19807116985@163.com>
2023-09-24 16:20:28 +0800

    基数排序

---
+++ 3/BucketSort.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 获取最大值的位数
+int maxBits(int* arr, size_t length) {
+    if (arr == NULL || length == 0) {
+        return 0;
+    }
+    int max = arr[0];
+    for (int i = 1; i < length; i++) {
+        max = arr[i] > max ? arr[i] : max;
+    }
+    int res = 0;
+    while (max != 0) {
+        max /= 10;
+        res++;
+    }
+    return res;
+}
+
+// 返回指定位上的数字
+int getDigit(int number, int d) {
+    for (int i = 1; i < d; i++) {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+// 基数排序
+void radixSort(int* arr, int L, int R, int maxBits) {
+    const int radix = 10;
+    // 有多少个数准备多少个辅助空间
+    int* bucket = (int*)malloc((R - L + 1) * sizeof(int));
+    if (bucket == NULL) {
+        // 处理内存分配失败的情况
+        exit(1);
+    }
+    // 数组最大值有几位就进出桶多少次
+    for (int i = 1; i <= maxBits; i++) {
+        // 基数计数
+        int count[radix] = {0};
+        for (int j = L; j <= R; j++) {
+            int digit = getDigit(arr[j], i);
+            count[digit]++;
+        }
+        // 将count更新为前缀和数组
+        for (int j = 1; j < radix; j++) {
+            count[j] += count[j - 1];
+        }
+        // 从右往左对当前位进行桶排序
+        for (int j = R; j >= L; j--) {
+            int digit = getDigit(arr[j], i);
+            bucket[count[digit] - 1] = arr[j];
+            count[digit]--;
+        }
+        for (int j = 0, k = L; k <= R; j++, k++) {
+            arr[k] = bucket[j];
+        }
+    }
+    free(bucket);
+    return;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 6, 0, 15, 32, 26, 9, 8, 6, 4};
+    size_t length = sizeof(arr) / sizeof(arr[0]);
+    radixSort(arr, 0, length - 1, maxBits(arr, length));
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
--- 3/HeapSort.c
+++ 3/HeapSort.c
@@ -1,5 +1,42 @@
-#include "HeapSort.h"
 #include <stdio.h>
+
+// 交换
+void swap(int* arr, int index1, int index2) {
+    int temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+    return;
+}
+
+// index的数向上检查
+void heapInsert(int* arr, int index) {
+    while (arr[index] > arr[(index - 1) >> 1]) {
+        swap(arr, index, (index - 1) >> 1);
+        index = (index - 1) >> 1;
+    }
+    return;
+}
+
+// index的数向下检查
+void heapify(int* arr, int index, int heapSize) {
+    int left = index * 2 + 1;
+    int largest;
+    while (left < heapSize) {
+        if (left + 1 < heapSize && arr[left + 1] > arr[left]) {
+            largest = left + 1;
+        } else {
+            largest = left;
+        }
+        largest = arr[index] > arr[largest] ? index : largest;
+        if (largest == index) {
+            break;
+        }
+        swap(arr, largest, index);
+        index = largest;
+        left = index * 2 + 1;
+    }
+    return;
+}
 
 void heapSort(int* arr, size_t length) {
     if (arr == NULL || length < 2) {
@@ -18,7 +55,7 @@ void heapSort(int* arr, size_t length) {
 }
 
 int main() {
-    int arr[] = {2, 5, 3, 4, 9, 8, 13, 56, 35, 21};
+    int arr[] = {2, 5, 1, 3, 4, 9, 8, 13, 56, 35, 21};
     size_t length = sizeof(arr) / sizeof(arr[0]);
     heapSort(arr, length);
     for (int i = 0; i < length; i++) {
